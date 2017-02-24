fae0f2b86601647776eb52b8f6e6061baf3c9b5a
Ryan Schlesinger <ryan@ryanschlesinger.com>
2017-02-24 15:07:39 -0800

    Stop supporting 2.2

--- 2.2/upgrade_bundler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# bundler is installed outside of GEM_HOME
-SYSTEM_GEM_HOME=/usr/local/lib/ruby/gems/$(ls /usr/local/lib/ruby/gems)
-gem uninstall -i ${SYSTEM_GEM_HOME} bundler
-gem install bundler -i ${SYSTEM_GEM_HOME} -v "$BUNDLER_VERSION"
